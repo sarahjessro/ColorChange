01d604ebcfa65c34b20773c617263f079fecacb6
Sarah <143215557+sarahjessro@users.noreply.github.com>
2024-07-10 20:39:06 -0400

    Add files via upload

---
+++ Color Changer/index.js
@@ -0,0 +1,10 @@
+function colorChange() {
+   document.getElementById("html").style.backgroundColor = "black";
+   document.getElementById("h1").style.color = "cadetblue"
+
+}
+
+function colorRemove() {
+    document.getElementById("html").style.backgroundColor = "white";
+    document.getElementById("h1").style.color = "black" 
+}
